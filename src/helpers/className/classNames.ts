8901712f647c2ce09f25f66a6d37324be497af59
Anton Komarnytskyi <anton.komarnytskyi@masterofcode.com>
2023-11-08 09:25:08 +0200

    initial commit

---
+++ src/helpers/className/classNames.ts
@@ -0,0 +1,7 @@
+type Mods = Record<string, boolean | string>;
+
+export const classNames = (cls: string, mods: Mods, additional: string[]): string => {
+  const filteredMods = Object.entries(mods).flatMap(([className, value]) => (value ? className : []));
+
+  return [cls, ...additional, ...filteredMods].join(' ');
+}
