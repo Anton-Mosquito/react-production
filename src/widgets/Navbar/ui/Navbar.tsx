dfa31a3aff07eb346b02862eae989411a7b60cad
Anton Komarnytskyi <anton.komarnytskyi@masterofcode.com>
2023-11-15 11:12:33 +0200

    add svg and files loaders add svg for buttons

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from  './Navbar.module.scss';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLinks';
+import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 
 interface NavbarProps {
   className?: string;
@@ -8,6 +9,7 @@ interface NavbarProps {
 export const Navbar = ({className}: NavbarProps) => {
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
+      <ThemeSwitcher/>
       <div className={cls.links}>
         <AppLink to={'/'} className={cls.mainLink} theme={AppLinkTheme.SECONDARY}>Main page</AppLink>
         <AppLink to={'/about'} theme={AppLinkTheme.SECONDARY}>About page</AppLink>
