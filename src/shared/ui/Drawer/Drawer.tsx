1f8866888fb364ab15c2c58e465954df1605da37
Anton Komarnytskyi <anton.komarnytskyi@masterofcode.com>
2024-03-04 09:28:56 +0200

    add useModal hook, upgrade drawer

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -4,23 +4,35 @@ import cls from './Drawer.module.scss'
 import { useTheme } from 'app/providers/ThemeProvider'
 import { Portal } from '../Portal/Portal'
 import { Overlay } from '../Overlay/Overlay'
+import { useModal } from 'shared/lib/hooks/useModal/useModal'
 
 interface DrawerProps extends PropsWithChildren {
   className?: string
   isOpen?: boolean
   onClose?: () => void
+  lazy?: boolean
 }
 
-export const Drawer = memo(({ className, children, isOpen, onClose }: DrawerProps): JSX.Element => {
+export const Drawer = memo(({ className, children, isOpen, onClose, lazy }: DrawerProps): JSX.Element | null => {
   const { theme } = useTheme()
+  const { isClosing, isMounted, close } = useModal({
+    animationDelay: 300,
+    isOpen,
+    onClose
+  })
 
   const mods: Mods = {
-    [cls.opened]: isOpen
+    [cls.opened]: isOpen,
+    [cls.isClosing]: isClosing
+  }
+
+  if (lazy && !isMounted) {
+    return null
   }
   return (
       <Portal >
           <div className={classNames(cls.Drawer, mods, [className, theme, 'app_drawer'])}>
-              <Overlay onClick={onClose} />
+              <Overlay onClick={close} />
               <div className={cls.content}>
                   { children}
               </div>
