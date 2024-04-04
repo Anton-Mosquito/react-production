caf61939c98c1fe925e50693f33e20385cce870c
Anton Komarnytskyi <anton.komarnytskyi@masterofcode.com>
2024-04-04 09:48:01 +0300

    Update dependencies and storybook configuration

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -1,4 +1,8 @@
 import type { StorybookConfig } from '@storybook/react-webpack5'
+import path from 'path';
+import { RuleSetRule, DefinePlugin } from 'webpack';
+import { buildCssLoader } from '../build/loaders/buildCssLoader';
+import { BuildPath } from '../build/types/config';
 
 const config: StorybookConfig = {
   stories: ['../../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -18,13 +22,67 @@ const config: StorybookConfig = {
   framework: {
     name: '@storybook/react-webpack5',
     options: {
-      builder: {
-        useSWC: true
-      }
+      builder: {}
     }
   },
   docs: {
     autodocs: 'tag'
-  }
+  },
+  swc: () => ({
+    jsc: {
+      transform: {
+        react: {
+          runtime: 'automatic',
+        },
+      },
+    },
+  }),
+  webpackFinal: async (config) => {
+    const paths: BuildPath = {
+      build: '',
+      html: '',
+      entry: '',
+      src: path.resolve(__dirname, '..', '..', 'src'),
+      locales: '',
+      buildLocales: ''
+    }
+  
+    if (config.resolve) {
+      config.resolve.modules?.push(paths.src)
+      config.resolve.extensions?.push('.ts', '.tsx')
+      config.resolve.alias = {
+        ...config.resolve?.alias,
+        '@': paths.src
+      }
+    }
+  
+    if (config.module?.rules) {
+      config.module.rules = (config.module?.rules as RuleSetRule[]).map((rule) => {
+        if (rule.test != null && (typeof rule.test === 'string' || rule.test instanceof RegExp)) {
+          const test = rule.test.toString()
+          if (test.includes('svg')) {
+            return { ...rule, exclude: /\.svg$/i };
+          }
+        }
+  
+        return rule
+      })
+  
+      config.module?.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack']
+      })
+  
+      config.module?.rules.push(buildCssLoader(true))
+    }
+  
+    config.plugins?.push(new DefinePlugin({
+      __IS_DEV__: JSON.stringify(true),
+      __API__: JSON.stringify('http://testapi.ua'),
+      __PROJECT__: JSON.stringify('storybook')
+    }))
+  
+    return config
+  },
 }
 export default config
